7dfda4af3e63bfe20e023dbc23f2c5af87e59b52
Zebra <zebra1573@163.com>
2025-07-27 23:27:44 +0800

    日期时间参数 & json参数 & 路径参数

--- springboot-web-req-resp/src/main/java/com/zebra/controller/RequestController.java
+++ springboot-web-req-resp/src/main/java/com/zebra/controller/RequestController.java
@@ -1,10 +1,10 @@
 package com.zebra.controller;
 
 import com.zebra.pojo.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -44,6 +44,27 @@ public class RequestController {
     @RequestMapping("/listParam")
     public String listParam(@RequestParam List<String> hobby) {
         System.out.println(hobby);
+        return "OK";
+    }
+
+    // 日期时间参数
+    @RequestMapping("/dateParam")
+    public String dateParam(@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updateTime) {
+        System.out.println(updateTime);
+        return "OK";
+    }
+
+    // json参数
+    @RequestMapping("/jsonParam")
+    public String jsonParam(@RequestBody User user) {
+        System.out.println(user);
+        return "OK";
+    }
+
+    // 路径参数
+    @RequestMapping("/path/{id}/{name}")
+    public String pathParam(@PathVariable Integer id, @PathVariable String name) {
+        System.out.println(id + ":" + name);
         return "OK";
     }
 }
