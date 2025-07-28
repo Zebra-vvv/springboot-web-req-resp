794c3b619075126056f91db5cb6629143cacf91d
Zebra <zebra1573@163.com>
2025-07-28 12:57:38 +0800

    init

---
+++ springtboot-mybatis-quickstart/src/test/java/com/zebra/SprintbootMybatisQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zebra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootMybatisQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
